11427d32a36939a9f9ae40369b167571685d6ee4
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-03 23:14:19 +0000

    ✍🏽 Added the test code for the breed cat

--- d4-api/breedFetcher.js
+++ d4-api/breedFetcher.js
@@ -1,25 +1,23 @@
 const request = require('request');
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const args = process.argv;
-const breedName = args[2];
-//console.log(breedName);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-
-request(url, (error, response, body) => {
-  // console.log(response.error);
-  //  console.log(body);
-  // console.log(response);
-  // console.log(response.body)
-  // console.log(error);
-  if (error) {
-    console.log(`Error:`,error);
-  } else {
+  request(url, (error, response, body) => {
+    if (error) {
+      // console.log(`Error:`, error);
+      callback(error, null);
+    }
     const catObj = JSON.parse(body);
+    if (catObj.length == 0) {
+      callback(null, "Invalid cat breed");
+      //console.log('Invalid cat breed');
+    } else {
+      callback(null,catObj[0].description);
+      //console.log(catObj);
+      //console.log(catObj[0].description);
+    }
 
-    //console.log(catObj);
-     console.log(catObj[0].description);
-  }
-});
+  });
+};
 
-// // console.log(typeof object)
+module.exports = { fetchBreedDescription };
