8956576f4145d70ec7a435267ca39e99405783e4
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-05 03:52:37 +0000

    ✍🏽 More with OOP and Extends

--- pizzaClass.js
+++ pizzaClass.js
@@ -35,8 +35,9 @@ let pizza = new Pizza('large', 'thin');
 
 class Person {
   // moved here b/c it was identical
-  constructor(name, quirkyFact) {
+  constructor(name, quirkyFact, email) {
     this.name = name;
+    this.email = email;
     this.quirkyFact = quirkyFact;
   }
 
@@ -47,15 +48,23 @@ class Person {
 }
 
 
+
+
 class Student extends Person {
   // stays in Student class since it's specific to students only
   enroll(cohort) {
     this.cohort = cohort;
   }
+  bio() {
+    return `I'm ${this.name} a student at Lighthouse Labs (aka Labber).`;
+  }
 }
 
 class Mentor extends Person {
   // specific to mentors
+  bio() {
+    return `I'am a mentor at Lighthouse Labs. My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
+  }
   goOnShift() {
     this.onShift = true;
   }
@@ -66,6 +75,11 @@ class Mentor extends Person {
   }
 }
 
+const Emeka = new Student('Emeka Dev,');
+console.log(Emeka.bio());
+
+const bob = new Mentor('Bob Ross','I like mountains way too much');
+console.log(bob.bio());
 
 
 
